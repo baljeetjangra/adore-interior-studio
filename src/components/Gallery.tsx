6d59567197493b50b0b2d963ce156d7f4dc9e2e8
Baljeet <baljeetjangraa@gmail.com>
2025-08-16 13:31:12 +0530

    metadata added

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -89,11 +89,11 @@ const Gallery = () => {
               <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-all duration-300 flex items-center justify-center">
                 <Eye className="w-8 h-8 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
               </div>
-              <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+              {/* <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
                 <p className="text-white text-sm font-medium opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                   {image.alt}
                 </p>
-              </div>
+              </div> */}
             </div>
           ))}
         </div>
